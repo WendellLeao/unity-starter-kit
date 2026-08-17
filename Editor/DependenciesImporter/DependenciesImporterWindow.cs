adb49bf63eb47b1119b777c932019922a79de7af
Wendell Leão <leaowendell@outlook.com>
2026-08-17 14:09:43 -0300

    chore: update Scene Search Window package reference

--- Editor/DependenciesImporter/DependenciesImporterWindow.cs
+++ Editor/DependenciesImporter/DependenciesImporterWindow.cs
@@ -22,7 +22,7 @@ namespace WendellLeao.StarterKit.Editor.DependenciesImporter
             ("com.wendellleao.screenservice", "https://github.com/WendellLeao/screen-service.git", "Screen Service"),
             ("com.wendellleao.audioservice", "https://github.com/WendellLeao/audio-service.git", "Audio Service"),
             ("com.wendellleao.scenebootstrap", "https://github.com/WendellLeao/scene-bootstrap.git", "Scene Bootstrap"),
-            ("com.wendellleao.sceneloader", "https://github.com/WendellLeao/scene-loader.git", "Scene Loader"),
+            ("com.wendellleao.scenesearchwindow", "https://github.com/WendellLeao/scene-search-window.git", "Scene Search Window"),
             ("com.cysharp.unitask", "https://github.com/Cysharp/UniTask.git?path=src/UniTask/Assets/Plugins/UniTask", "UniTask")
         };
 
